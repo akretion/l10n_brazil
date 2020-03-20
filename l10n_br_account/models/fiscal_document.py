21bad0cc7266c895853dbef67e8c8d463487e22e
Luis Felipe Mileo <mileo@kmee.com.br>
2020-03-20 16:15:59 -0300

    [NEW] Generate account moves from l10n_br_fiscal
    Signed-off-by: Luis Felipe Mileo <mileo@kmee.com.br>
    
    Signed-off-by: Luis Felipe Mileo <mileo@kmee.com.br>

--- l10n_br_account/models/fiscal_document.py
+++ l10n_br_account/models/fiscal_document.py
@@ -3,7 +3,7 @@
 # Copyright (C) 2020 - TODAY Luis Felipe Mileo - KMEE
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class FiscalDocument(models.Model):
