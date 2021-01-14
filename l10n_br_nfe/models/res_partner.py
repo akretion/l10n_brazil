bf5be93a83ffefa1a9f2a14ac411b09dd5fe6029
Marcel Savegnago <marcel.savegnago@gmail.com>
2021-01-14 01:52:28 -0300

    [RFC] refactor res.partner class

--- l10n_br_nfe/models/res_partner.py
+++ l10n_br_nfe/models/res_partner.py
@@ -6,32 +6,27 @@ from odoo import api, fields
 from odoo.addons.spec_driven_model.models import spec_models
 
 
-class ResPartner(spec_models.SpecModel):
-    # NOTE TODO
-    # dest has a m2o tendereco. tlocal and tendereco are really similar...
-    # what happen to m2o to tendereco if we don't inherit from tendereco?
-    # should we stack tendereco from dest? will m2o to tendereco work?
-    # can we use related fields and context views to avoid troubles?
+class ResPartner(spec_models.StackedModel):
     _name = 'res.partner'
-    _inherit = ['res.partner', 'nfe.40.tendereco',
-                'nfe.40.tlocal', 'nfe.40.dest', 'nfe.40.tenderemi']
-    _nfe_search_keys = ['nfe40_CNPJ', 'nfe40_CPF', 'nfe40_xNome']
-
-    @api.model
-    def _prepare_import_dict(self, values, defaults={}):
-        values = super()._prepare_import_dict(values)
-        if not values.get('name') and values.get('legal_name'):
-            values['name'] = values['legal_name']
-        return values
+    _inherit = ['res.partner', 'nfe.40.dest']
+    _stacked = 'nfe.40.dest'
+    _field_prefix = 'nfe40_'
+    _schema_name = 'nfe'
+    _schema_version = '4.0.0'
+    _odoo_module = 'l10n_br_nfe'
+    _spec_module = 'odoo.addons.l10n_br_nfe_spec.models.v4_00.leiauteNFe'
+    _spec_tab_name = 'NFe'
+    # all m2o below this level will be stacked even if not required:
+    _rec_name = 'nfe40_xNome'
 
     # nfe.40.tlocal / nfe.40.enderEmit / 'nfe.40.enderDest
-    # TODO: may be not store=True -> then override match
     nfe40_CNPJ = fields.Char(compute='_compute_nfe_data',
-                             inverse='_inverse_nfe40_CNPJ',
-                             store=True)
+                             inverse='_inverse_nfe40_CNPJ')
     nfe40_CPF = fields.Char(compute='_compute_nfe_data',
-                            inverse='_inverse_nfe40_CNPJ',
-                            store=True)
+                            inverse='_inverse_nfe40_CNPJ')
+    nfe40_idEstrangeiro = fields.Char(compute='_compute_nfe_data',
+                                      inverse='_inverse_nfe40_CNPJ')
+
     nfe40_xLgr = fields.Char(related='street', readonly=False)
     nfe40_nro = fields.Char(related='street_number', readonly=False)
     nfe40_xCpl = fields.Char(related='street2', readonly=False)
@@ -49,13 +44,13 @@ class ResPartner(spec_models.SpecModel):
     nfe40_fone = fields.Char(related='phone', readonly=False)  # TODO mobile?
 
     # nfe.40.dest
-#    nfe40_idEstrangeiro = fields.Char(
     nfe40_xNome = fields.Char(related='legal_name')
     nfe40_enderDest = fields.Many2one('res.partner',
                                       compute='_compute_nfe40_enderDest')
     nfe40_indIEDest = fields.Selection(related='ind_ie_dest')
     nfe40_IE = fields.Char(related='inscr_est')
     nfe40_ISUF = fields.Char(related='suframa')
+    nfe40_IM = fields.Char(related='inscr_mun')
     nfe40_email = fields.Char(related='email')
 
     @api.multi
