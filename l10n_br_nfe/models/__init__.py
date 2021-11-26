f09ade84075e5071bd2daf049522fe3b6b747a0b
Renato Lima <renato.lima@akretion.com>
2021-11-26 12:53:58 -0300

    [ADD] NCM _nfe_search_keys

--- l10n_br_nfe/models/__init__.py
+++ l10n_br_nfe/models/__init__.py
@@ -12,6 +12,7 @@ from . import document
 from . import document_line
 from . import res_city
 from . import res_config_settings
+from . import ncm
 from . import cfop
 from . import invalidate_number
 from . import spec_mixin
