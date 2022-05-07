Escodoo/l10n-brazil

[FIX] l10n_br_account: fix copy method

l10n_br_account/models/account_invoice.py
@@ -247,7 +247,7 @@ class AccountMove(models.Model):
     def copy(self, default=None):
         default = default or {}
         if self.document_type_id:
-            default["line_ids"] = False
+            default["invoice_line_ids"] = False
         return super().copy(default)
 
     def _recompute_tax_lines(self, recompute_tax_base_amount=False):

akretion/l10n_brazil

l10n_br_account/models/account_invoice.py
@@ -246,7 +246,7 @@ class AccountMove(models.Model):
     def copy(self, default=None):
         default = default or {}
         if self.document_type_id:
-            default["line_ids"] = False
+            default["fiscal_line_ids"] = False
         return super().copy(default)
 
     def _recompute_tax_lines(self, recompute_tax_base_amount=False):