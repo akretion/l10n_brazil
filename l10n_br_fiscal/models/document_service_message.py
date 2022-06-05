84d0982297a8a342e3ab987b2b4169cc0008dd37
Renato Lima <renato.lima@akretion.com>
2022-06-05 00:46:52 -0300

    [ADD] new objet fiscal document web service messages

---
+++ l10n_br_fiscal/models/document_service_message.py
@@ -0,0 +1,33 @@
+# Copyright (C) 2022  Renato Lima - Akretion <renato.lima@akretion.com.br>
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
+from odoo import fields, models
+
+
+class DocumentServiceMessage(models.Model):
+    _name = "l10n_br_fiscal.document.service.message"
+    _inherit = "l10n_br_fiscal.data.abstract"
+    _description = "Generic Document Service Message"
+
+    name = fields.Char(string="Name", required=True, index=True)
+
+    description = fields.Text(string="Description", required=True, index=True)
+
+    document_service_id = fields.Many2one(
+        comodel_name="l10n_br_fiscal.document.service",
+        string="Fiscal Document Service",
+    )
+
+    document_type_id = fields.Many2one(
+        comodel_name="l10n_br_fiscal.document.type",
+        related="document_service_id.document_type_id",
+        string="Document Type",
+    )
+
+    message_type = fields.Selection(
+        selection=[
+            ('request', 'Request'),
+            ('response', 'Response')
+        ],
+        string="Message Type",
+    )
