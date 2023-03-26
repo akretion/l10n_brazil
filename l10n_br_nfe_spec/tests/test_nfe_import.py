felipemotter/l10n-brazil

[REF] adapted in/out tests for xsdata

l10n_br_nfe_spec/tests/test_nfe_import.py
@@ -6,7 +6,8 @@ from datetime import datetime
 
 import nfelib
 import pkg_resources
-from nfelib.v4_00 import leiauteNFe_sub as nfe_sub
+from nfelib.bindings.nfe.v4_0.leiaute_nfe_v4_00 import TnfeProc
+from xsdata.formats.dataclass.parsers import XmlParser
 
 from odoo import api
 from odoo.tests import SavepointCase
@@ -24,17 +25,23 @@ def build_fake(self, node, create=False):
 
 @api.model
 def build_attrs_fake(self, node, create_m2o=False):
+    """
+    Similar to build_attrs from spec_driven_model but simpler: assuming
+    generated abstract mixins are not injected into concrete Odoo models.
+    """
     fields = self.fields_get()
     vals = self.default_get(fields.keys())
-    for attr in node.member_data_items_:
-        value = getattr(node, attr.get_name())
+    for fname, fspec in node.__dataclass_fields__.items():
+        value = getattr(node, fname)
         if value is None:
             continue
-        key = "nfe40_%s" % (attr.get_name(),)
+        key = "%s%s" % (
+            self._field_prefix,
+            fspec.metadata.get("name", fname),
+        )
         if (
-            attr.get_child_attrs().get("type") is None
-            or attr.get_child_attrs().get("type") == "xs:string"
-        ):
+            fspec.type == str or not any(["." in str(i) for i in fspec.type.__args__])
+        ) and not str(fspec.type).startswith("typing.List"):
             # SimpleType
             if fields[key]["type"] == "datetime":
                 if "T" in value:
@@ -46,18 +53,27 @@ def build_attrs_fake(self, node, create_m2o=False):
             vals[key] = value
 
         else:
+            if hasattr(fspec.type.__args__[0], "__name__"):
+                binding_type = fspec.type.__args__[0].__name__
+            else:
+                binding_type = fspec.type.__args__[0].__forward_arg__
+
             # ComplexType
             if fields.get(key) and fields[key].get("related"):
                 key = fields[key]["related"][0]
                 comodel_name = fields[key]["relation"]
             else:
-                clean_type = attr.get_child_attrs()["type"].replace("Type", "").lower()
-                comodel_name = "nfe.40.%s" % (clean_type,)
+                clean_type = binding_type.lower()  # TODO double check
+                comodel_name = "%s.%s.%s" % (
+                    self._schema_name,
+                    self._schema_version.replace(".", "")[0:2],
+                    clean_type.split(".")[-1],
+                )
             comodel = self.env.get(comodel_name)
             if comodel is None:  # example skip ICMS100 class
                 continue
 
-            if attr.get_container() == 0:
+            if not str(fspec.type).startswith("typing.List"):
                 # m2o
                 new_value = comodel.build_attrs_fake(
                     value,
@@ -71,7 +87,7 @@ def build_attrs_fake(self, node, create_m2o=False):
                     vals[key] = self.match_or_create_m2o_fake(
                         comodel, new_value, create_m2o
                     )
-            elif attr.get_container() == 1:
+            else:  # if attr.get_container() == 1:
                 # o2m
                 lines = []
                 for line in [li for li in value if li]:
@@ -103,7 +119,6 @@ spec_models.NfeSpecMixin.match_or_create_m2o_fake = match_or_create_m2o_fake
 class NFeImportTest(SavepointCase):
     def test_import_nfe1(self):
         res_items = (
-            "..",
             "tests",
             "nfe",
             "v4_00",
@@ -112,15 +127,15 @@ class NFeImportTest(SavepointCase):
         )
         resource_path = "/".join(res_items)
         nfe_stream = pkg_resources.resource_stream(nfelib.__name__, resource_path)
-        nfe_binding = nfe_sub.parse(nfe_stream, silence=True)
-        nfe = self.env["nfe.40.infnfe"].build_fake(nfe_binding.infNFe, create=False)
+        parser = XmlParser()
+        binding = parser.from_string(nfe_stream.read().decode(), TnfeProc)
+        nfe = self.env["nfe.40.infnfe"].build_fake(binding.nfe.inf_nfe, create=False)
         self.assertEqual(nfe.nfe40_emit.nfe40_CNPJ, "75335849000115")
         self.assertEqual(len(nfe.nfe40_det), 3)
         self.assertEqual(nfe.nfe40_det[0].nfe40_prod.nfe40_cProd, "880945")
 
     def test_import_nfe2(self):
         res_items = (
-            "..",
             "tests",
             "nfe",
             "v4_00",
@@ -129,8 +144,9 @@ class NFeImportTest(SavepointCase):
         )
         resource_path = "/".join(res_items)
         nfe_stream = pkg_resources.resource_stream(nfelib.__name__, resource_path)
-        nfe_binding = nfe_sub.parse(nfe_stream, silence=True)
-        nfe = self.env["nfe.40.infnfe"].build_fake(nfe_binding.infNFe, create=False)
+        parser = XmlParser()
+        binding = parser.from_string(nfe_stream.read().decode(), TnfeProc)
+        nfe = self.env["nfe.40.infnfe"].build_fake(binding.nfe.inf_nfe, create=False)
         self.assertEqual(nfe.nfe40_emit.nfe40_CNPJ, "34128745000152")
         self.assertEqual(len(nfe.nfe40_det), 16)
         self.assertEqual(nfe.nfe40_det[0].nfe40_prod.nfe40_cProd, "1094")

akretion/l10n_brazil

l10n_br_nfe_spec/tests/test_nfe_import.py
@@ -6,12 +6,18 @@ from datetime import datetime
 
 import nfelib
 import pkg_resources
-from nfelib.v4_00 import leiauteNFe_sub as nfe_sub
+from nfelib.bindings.nfe.v4_0.leiaute_nfe_v4_00 import TnfeProc
+from xsdata.formats.dataclass.parsers import XmlParser
 
-from odoo import api
+from odoo import api, models
 from odoo.tests import SavepointCase
 
 from ..models import spec_models
+from ..models.v4_0 import (
+    leiaute_cons_sit_nfe_v4_00,
+    leiaute_cons_stat_serv_v4_00,
+    leiaute_inut_nfe_v4_00,
+)
 
 tz_datetime = re.compile(r".*[-+]0[0-9]:00$")
 
@@ -24,17 +30,23 @@ def build_fake(self, node, create=False):
 
 @api.model
 def build_attrs_fake(self, node, create_m2o=False):
+    """
+    Similar to build_attrs from spec_driven_model but simpler: assuming
+    generated abstract mixins are not injected into concrete Odoo models.
+    """
     fields = self.fields_get()
     vals = self.default_get(fields.keys())
-    for attr in node.member_data_items_:
-        value = getattr(node, attr.get_name())
+    for fname, fspec in node.__dataclass_fields__.items():
+        value = getattr(node, fname)
         if value is None:
             continue
-        key = "nfe40_%s" % (attr.get_name(),)
+        key = "%s%s" % (
+            self._field_prefix,
+            fspec.metadata.get("name", fname),
+        )
         if (
-            attr.get_child_attrs().get("type") is None
-            or attr.get_child_attrs().get("type") == "xs:string"
-        ):
+            fspec.type == str or not any(["." in str(i) for i in fspec.type.__args__])
+        ) and not str(fspec.type).startswith("typing.List"):
             # SimpleType
             if fields[key]["type"] == "datetime":
                 if "T" in value:
@@ -46,18 +58,27 @@ def build_attrs_fake(self, node, create_m2o=False):
             vals[key] = value
 
         else:
+            if hasattr(fspec.type.__args__[0], "__name__"):
+                binding_type = fspec.type.__args__[0].__name__
+            else:
+                binding_type = fspec.type.__args__[0].__forward_arg__
+
             # ComplexType
             if fields.get(key) and fields[key].get("related"):
                 key = fields[key]["related"][0]
                 comodel_name = fields[key]["relation"]
             else:
-                clean_type = attr.get_child_attrs()["type"].replace("Type", "").lower()
-                comodel_name = "nfe.40.%s" % (clean_type,)
+                clean_type = binding_type.lower()  # TODO double check
+                comodel_name = "%s.%s.%s" % (
+                    self._schema_name,
+                    self._schema_version.replace(".", "")[0:2],
+                    clean_type.split(".")[-1],
+                )
             comodel = self.env.get(comodel_name)
             if comodel is None:  # example skip ICMS100 class
                 continue
 
-            if attr.get_container() == 0:
+            if not str(fspec.type).startswith("typing.List"):
                 # m2o
                 new_value = comodel.build_attrs_fake(
                     value,
@@ -71,7 +92,7 @@ def build_attrs_fake(self, node, create_m2o=False):
                     vals[key] = self.match_or_create_m2o_fake(
                         comodel, new_value, create_m2o
                     )
-            elif attr.get_container() == 1:
+            else:  # if attr.get_container() == 1:
                 # o2m
                 lines = []
                 for line in [li for li in value if li]:
@@ -103,7 +124,6 @@ spec_models.NfeSpecMixin.match_or_create_m2o_fake = match_or_create_m2o_fake
 class NFeImportTest(SavepointCase):
     def test_import_nfe1(self):
         res_items = (
-            "..",
             "tests",
             "nfe",
             "v4_00",
@@ -112,15 +132,15 @@ class NFeImportTest(SavepointCase):
         )
         resource_path = "/".join(res_items)
         nfe_stream = pkg_resources.resource_stream(nfelib.__name__, resource_path)
-        nfe_binding = nfe_sub.parse(nfe_stream, silence=True)
-        nfe = self.env["nfe.40.infnfe"].build_fake(nfe_binding.infNFe, create=False)
+        parser = XmlParser()
+        binding = parser.from_string(nfe_stream.read().decode(), TnfeProc)
+        nfe = self.env["nfe.40.infnfe"].build_fake(binding.NFe.infNFe, create=False)
         self.assertEqual(nfe.nfe40_emit.nfe40_CNPJ, "75335849000115")
         self.assertEqual(len(nfe.nfe40_det), 3)
         self.assertEqual(nfe.nfe40_det[0].nfe40_prod.nfe40_cProd, "880945")
 
     def test_import_nfe2(self):
         res_items = (
-            "..",
             "tests",
             "nfe",
             "v4_00",
@@ -129,8 +149,20 @@ class NFeImportTest(SavepointCase):
         )
         resource_path = "/".join(res_items)
         nfe_stream = pkg_resources.resource_stream(nfelib.__name__, resource_path)
-        nfe_binding = nfe_sub.parse(nfe_stream, silence=True)
-        nfe = self.env["nfe.40.infnfe"].build_fake(nfe_binding.infNFe, create=False)
+        parser = XmlParser()
+        binding = parser.from_string(nfe_stream.read().decode(), TnfeProc)
+        nfe = self.env["nfe.40.infnfe"].build_fake(binding.NFe.infNFe, create=False)
         self.assertEqual(nfe.nfe40_emit.nfe40_CNPJ, "34128745000152")
         self.assertEqual(len(nfe.nfe40_det), 16)
         self.assertEqual(nfe.nfe40_det[0].nfe40_prod.nfe40_cProd, "1094")
+
+    def test_init_all(self):
+        for mod in [
+            leiaute_cons_sit_nfe_v4_00,
+            leiaute_cons_stat_serv_v4_00,
+            leiaute_inut_nfe_v4_00,
+        ]:
+            for _klass_name, klass in mod.__dict__.items():
+                if isinstance(klass, type):
+                    self.assertTrue(issubclass(klass, models.AbstractModel))
+                    klass()