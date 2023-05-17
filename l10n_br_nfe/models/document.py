felipemotter/l10n-brazil

[REF] doc xsdata - serialize/render update

l10n_br_nfe/models/document.py
@@ -3,6 +3,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 import base64
+from dataclasses import dataclass
 import logging
 import re
 import string
@@ -16,7 +17,7 @@ from erpbrasil.edoc.nfe import NFe as edoc_nfe
 from erpbrasil.edoc.pdf import base
 from erpbrasil.transmissao import TransmissaoSOAP
 from lxml import etree
-from nfelib.v4_00 import leiauteNFe_sub as nfe_sub, retEnviNFe as leiauteNFe
+from nfelib.nfe.v4_0.nfe_v4_00 import Nfe
 from requests import Session
 
 from odoo import _, api, fields
@@ -74,7 +75,7 @@ class NFe(spec_models.StackedModel):
     _schema_name = "nfe"
     _schema_version = "4.0.0"
     _odoo_module = "l10n_br_nfe"
-    _spec_module = "odoo.addons.l10n_br_nfe_spec.models.v4_00.leiauteNFe"
+    _spec_module = "odoo.addons.l10n_br_nfe_spec.models.v4_0.leiaute_nfe_v4_00"
     _spec_tab_name = "NFe"
     _nfe_search_keys = ["nfe40_Id"]
 
@@ -629,12 +630,8 @@ class NFe(spec_models.StackedModel):
             filter_processador_edoc_nfe
         ):
             inf_nfe = record.export_ds()[0]
-
-            tnfe = leiauteNFe.TNFe(infNFe=inf_nfe, infNFeSupl=None, Signature=None)
-            tnfe.original_tagname_ = "NFe"
-
-            edocs.append(tnfe)
-
+            nfe = Nfe(inf_nfe=inf_nfe, inf_nfe_supl=None, signature=None)
+            edocs.append(nfe)
         return edocs
 
     def _processador(self):
@@ -659,11 +656,9 @@ class NFe(spec_models.StackedModel):
         super()._document_export()
         for record in self.filtered(filter_processador_edoc_nfe):
             record._export_fields_pagamentos()
-            edoc = record.serialize()[0]
+            edoc_binding = record.serialize()[0]
             processador = record._processador()
-            xml_file = processador._generateds_to_string_etree(
-                edoc, pretty_print=pretty_print
-            )[0]
+            xml_file = self._render_edoc(edoc_binding)
             _logger.debug(xml_file)
             event_id = self.event_ids.create_event_save_xml(
                 company_id=self.company_id,
@@ -675,8 +670,21 @@ class NFe(spec_models.StackedModel):
                 document_id=self,
             )
             record.authorization_event_id = event_id
-            xml_assinado = processador.assina_raiz(edoc, edoc.infNFe.Id)
+            # TODO copy decent assina_raiz method here
+            xml_assinado = "TODO" # processador.assina_raiz(edoc, edoc.InfNfe.id)
             self._valida_xml(xml_assinado)
+
+    def _render_edoc(self, edoc_binding, pretty_print=True):
+        "used to be in erpbrasil.edoc, but hacked here for xsdata"
+        from xsdata.formats.dataclass.serializers.config import SerializerConfig
+        from xsdata.formats.dataclass.serializers import XmlSerializer
+        from nfelib.nfe.v4_0.proc_nfe_v4_00  import NfeProc
+        serializer = XmlSerializer(config=SerializerConfig(pretty_print=True))
+        xml = serializer.render(
+            obj=edoc_binding,
+            ns_map={None:"http://www.portalfiscal.inf.br/nfe"}
+        )
+        return xml
 
     def atualiza_status_nfe(self, infProt, xml_file):
         self.ensure_one()
@@ -715,6 +723,7 @@ class NFe(spec_models.StackedModel):
         self._change_state(state)
 
     def _valida_xml(self, xml_file):
+        return True  # FIXME adapt for xsdata
         self.ensure_one()
         erros = nfe_sub.schema_validation(StringIO(xml_file))
         erros = "\n".join(erros)

akretion/l10n_brazil

l10n_br_nfe/models/document.py
@@ -16,7 +16,8 @@ from erpbrasil.edoc.nfe import NFe as edoc_nfe
 from erpbrasil.edoc.pdf import base
 from erpbrasil.transmissao import TransmissaoSOAP
 from lxml import etree
-from nfelib.v4_00 import leiauteNFe_sub as nfe_sub, retEnviNFe as leiauteNFe
+from nfelib.nfe.bindings.v4_0.nfe_v4_00 import Nfe
+from nfelib.v4_00 import leiauteNFe_sub as nfe_sub  # legacy schema validation
 from requests import Session
 
 from odoo import _, api, fields
@@ -704,12 +705,8 @@ class NFe(spec_models.StackedModel):
             filter_processador_edoc_nfe
         ):
             inf_nfe = record.export_ds()[0]
-
-            tnfe = leiauteNFe.TNFe(infNFe=inf_nfe, infNFeSupl=None, Signature=None)
-            tnfe.original_tagname_ = "NFe"
-
-            edocs.append(tnfe)
-
+            nfe = Nfe(infNFe=inf_nfe, infNFeSupl=None, signature=None)
+            edocs.append(nfe)
         return edocs
 
     def _processador(self):
@@ -733,11 +730,9 @@ class NFe(spec_models.StackedModel):
     def _document_export(self, pretty_print=True):
         result = super()._document_export()
         for record in self.filtered(filter_processador_edoc_nfe):
-            edoc = record.serialize()[0]
+            edoc_binding = record.serialize()[0]
             processador = record._processador()
-            xml_file = processador._generateds_to_string_etree(
-                edoc, pretty_print=pretty_print
-            )[0]
+            xml_file = self._render_edoc(edoc_binding)
             _logger.debug(xml_file)
             event_id = self.event_ids.create_event_save_xml(
                 company_id=self.company_id,
@@ -749,9 +744,22 @@ class NFe(spec_models.StackedModel):
                 document_id=self,
             )
             record.authorization_event_id = event_id
-            xml_assinado = processador.assina_raiz(edoc, edoc.infNFe.Id)
+            # TODO copy decent assina_raiz method here
+            xml_assinado = "TODO" # processador.assina_raiz(edoc, edoc.InfNfe.id)
             self._valida_xml(xml_assinado)
         return result
+
+    def _render_edoc(self, edoc_binding, pretty_print=True):
+        "used to be in erpbrasil.edoc, but hacked here for xsdata"
+        from xsdata.formats.dataclass.serializers.config import SerializerConfig
+        from xsdata.formats.dataclass.serializers import XmlSerializer
+        from nfelib.nfe.v4_0.proc_nfe_v4_00  import NfeProc
+        serializer = XmlSerializer(config=SerializerConfig(pretty_print=True))
+        xml = serializer.render(
+            obj=edoc_binding,
+            ns_map={None:"http://www.portalfiscal.inf.br/nfe"}
+        )
+        return xml
 
     def atualiza_status_nfe(self, infProt, xml_file):
         self.ensure_one()