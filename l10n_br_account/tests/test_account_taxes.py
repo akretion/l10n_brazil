OCA/l10n-brazil

[12.0][FIX] Change l10n_br_simple_coa to l10n_br_coa_simple
Somo a troca do nome. Falta o script de migração

l10n_br_account/tests/test_account_taxes.py
@@ -8,14 +8,14 @@ class TestAccountTaxes(TransactionCase):
     def setUp(self):
         super(TestAccountTaxes, self).setUp()
 
-        self.l10n_br_simple_coa = self.env.ref(
-            'l10n_br_simple_coa.l10n_br_simple_chart_template')
+        self.l10n_br_coa_simple = self.env.ref(
+            'l10n_br_coa_simple.l10n_br_simple_chart_template')
         self.l10n_br_company = self.env['res.company'].create({
             'name': 'Empresa Teste do Plano de Contas Simplificado'})
 
         self.env.user.company_ids += self.l10n_br_company
         self.env.user.company_id = self.l10n_br_company
-        self.l10n_br_simple_coa.try_loading_for_current_company()
+        self.l10n_br_coa_simple.try_loading_for_current_company()
 
     def test_account_taxes(self):
         """Test if account taxes are related with fiscal taxes"""

akretion/l10n_brazil

l10n_br_account/tests/test_account_taxes.py
@@ -8,14 +8,14 @@ class TestAccountTaxes(TransactionCase):
     def setUp(self):
         super(TestAccountTaxes, self).setUp()
 
-        self.l10n_br_simple_coa = self.env.ref(
-            'l10n_br_simple_coa.l10n_br_simple_chart_template')
+        self.l10n_br_coa_simple = self.env.ref(
+            'l10n_br_coa_simple.l10n_br_coa_simple_chart_template')
         self.l10n_br_company = self.env['res.company'].create({
             'name': 'Empresa Teste do Plano de Contas Simplificado'})
 
         self.env.user.company_ids += self.l10n_br_company
         self.env.user.company_id = self.l10n_br_company
-        self.l10n_br_simple_coa.try_loading_for_current_company()
+        self.l10n_br_coa_simple.try_loading_for_current_company()
 
     def test_account_taxes(self):
         """Test if account taxes are related with fiscal taxes"""