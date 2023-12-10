OCA/l10n-brazil

[REF] l10n_br_fiscal: Moved Comments creation method to be also used when l10n_br_account are not installed.

l10n_br_fiscal/models/document_workflow.py
@@ -300,6 +300,12 @@ class DocumentWorkflow(models.AbstractModel):
 
     def _document_confirm(self):
         if self.issuer == DOCUMENT_ISSUER_COMPANY:
+            if not self.comment_ids and self.fiscal_operation_id.comment_ids:
+                self.comment_ids |= self.fiscal_operation_id.comment_ids
+
+            for line in self.line_ids:
+                if not line.comment_ids and line.fiscal_operation_line_id.comment_ids:
+                    line.comment_ids |= line.fiscal_operation_line_id.comment_ids
             self._change_state(SITUACAO_EDOC_A_ENVIAR)
 
     def action_document_confirm(self):

akretion/l10n_brazil

l10n_br_fiscal/models/document_workflow.py
@@ -302,6 +302,12 @@ class DocumentWorkflow(models.AbstractModel):
 
     def _document_confirm(self):
         if self.issuer == DOCUMENT_ISSUER_COMPANY:
+            if not self.comment_ids and self.fiscal_operation_id.comment_ids:
+                self.comment_ids |= self.fiscal_operation_id.comment_ids
+
+            for line in self.fiscal_line_ids:
+                if not line.comment_ids and line.fiscal_operation_line_id.comment_ids:
+                    line.comment_ids |= line.fiscal_operation_line_id.comment_ids
             self._change_state(SITUACAO_EDOC_A_ENVIAR)
 
     def action_document_confirm(self):