OCA/l10n-brazil

l10n_br_account: adapt compute_taxes

l10n_br_account/models/account_invoice_line.py
@@ -78,6 +78,8 @@ class AccountInvoiceLine(models.Model):
 
     partner_company_type = fields.Selection(related="partner_id.company_type")
 
+    ind_final = fields.Selection(related="invoice_id.ind_final")
+
     fiscal_genre_code = fields.Char(
         related="fiscal_genre_id.code",
         string="Fiscal Product Genre Code",

akretion/l10n_brazil

l10n_br_account/models/account_invoice_line.py
@@ -63,6 +63,8 @@ class AccountMoveLine(models.Model):
 
     partner_company_type = fields.Selection(related="partner_id.company_type")
 
+    ind_final = fields.Selection(related="move_id.ind_final")
+
     fiscal_genre_code = fields.Char(
         related="fiscal_genre_id.code",
         string="Fiscal Product Genre Code",