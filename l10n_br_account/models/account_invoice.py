OCA/l10n-brazil

[FIX] l10n_br_account: dummy fiscal document line correction

l10n_br_account/models/account_invoice.py
@@ -223,6 +223,8 @@ class AccountInvoice(models.Model):
         default = default or {}
         if self.document_type_id:
             default["line_ids"] = False
+        else:
+            default["line_ids"] = self.line_ids[0]
         return super().copy(default)
 
     @api.one

akretion/l10n_brazil

l10n_br_account/models/account_invoice.py
@@ -251,6 +251,8 @@ class AccountMove(models.Model):
         default = default or {}
         if self.document_type_id:
             default["fiscal_line_ids"] = False
+        else:
+            default["fiscal_line_ids"] = self.line_ids[0]
         return super().copy(default)
 
     @api.model